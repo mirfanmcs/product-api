ef6f19c82e6c8a1c92d0782c38d15b17b2dcbfb9
Muhammad Irfan <mirfan2@hotmail.com>
2022-12-07 14:24:16 +1100

    update

--- src/main/java/com/irfanstore/product/controller/InfoController.java
+++ src/main/java/com/irfanstore/product/controller/InfoController.java
@@ -40,7 +40,7 @@ public class InfoController {
     @GetMapping("/api/greeting")
     public ResponseEntity<String> greeting() {
 
-        return new ResponseEntity<String>("Hello world", HttpStatus.OK);
+        return new ResponseEntity<String>("Hello world2", HttpStatus.OK);
     }
 
 }
